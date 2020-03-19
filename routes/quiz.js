0556bbdf107a439638740d01608f2724bad6fa79
PeeLeN-PL <peelen10@wp.pl>
2020-03-19 14:30:11 +0100

    quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('quiz', { title: 'quiz' });
+  const show = !req.session.vote;
+  const quizess = Quiz.find();
+  quizess.exec((err, data) => {
+    let sum = 0;
+    data.forEach((item) => {
+      sum += item.vote
+    })
+    res.render('quiz', { title: 'quiz', data, sum, show });
+  });
+});
+
+router.post('/', (req, res) => {
+  const id = req.body.quiz;
+  const quizItem = Quiz.findOne({
+    _id: id
+  });
+  quizItem.exec((err, data) => {
+    ++data.vote;
+    data.save((err) => {
+      req.session.vote = 1;
+      res.redirect('/quiz')
+    });
+  });
 });
 
 module.exports = router;
