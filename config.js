b5a6871ebc9b11fffabe31bf75fef82317ca37a4
PeeLeN-PL <peelen10@wp.pl>
2020-03-19 22:03:42 +0100

    add config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:ESZnDkHWNhvJV2nH@cluster0-idy1z.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
